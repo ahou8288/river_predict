41d7af8fcdd6b9ddf68ca7b6814e0ab70f559bd2
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-12-02 21:12:57 -0300

    printing info about gaps

--- plot_nymboida.py
+++ plot_nymboida.py
@@ -27,20 +27,40 @@ levels = sql('''
     ORDER BY year,month,day
     '''.format(nymbo_gauge))
 
-subsection = 0
-gaps = []
-sections = []
-for i in range(len(rain) - 1):
-    today = datetime.date(rain[i][0], rain[i][1], rain[i][2])
-    tomorrow = datetime.date(rain[i + 1][0], rain[i + 1][1], rain[i + 1][2])
-    subsection += 1
-    gap = (tomorrow - today).days
-    if gap > 1:
-        sections.append(subsection)
-        subsection = 0
-        gaps.append(gap)
+def find_gaps(data):
+    subsection = 1
+    gaps = []
+    sections = []
+    for i in range(len(data) - 1):
+        today = datetime.date(data[i][0], data[i][1], data[i][2])
+        tomorrow = datetime.date(data[i + 1][0], data[i + 1][1], data[i + 1][2])
+        gap = (tomorrow - today).days
+        subsection += 1
+        if gap > 1:
+            sections.append(subsection)
+            subsection = 0
+            gaps.append(gap)
+    sections.append(subsection)
+    return (gaps,sections)
+    # return (sorted(gaps),sorted(sections))
 
-print(sorted(gaps))
-print(sorted(sections))
-# print(len(rain))
-# print(len(levels))
+rain_gaps,rain_sections = find_gaps(rain)
+levels_gaps,levels_sections = find_gaps(levels)
+
+print('len levels')
+print(len(levels))
+# print(sum(levels_sections))
+print('len rain')
+print(len(rain))
+# print(sum(rain_sections))
+
+print('rain_gaps')
+print(rain_gaps)
+print('rain_sections')
+print(rain_sections)
+
+print('levels_gaps')
+print(levels_gaps)
+print('levels_sections')
+print(levels_sections)
+
