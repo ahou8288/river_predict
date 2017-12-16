d0251f2681d674925ead0b36e0ab3c3568980a05
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-12-16 17:04:24 -0300

    Decided against interpolation

--- load_window_data.py
+++ load_window_data.py
@@ -21,15 +21,3 @@ for index, entry in enumerate(csv_data):
     csv_data[index][1] = float(entry[1])
     csv_data[index][2] = float(entry[2])
 # print(csv_data)
-
-# Interpolate missing data
-interpolation_limit = 2
-row_index = 0
-should_interpolate = [False] * len(csv_data)
-print(should_interpolate)
-for col_index in range(1, 3):
-    for row_index in range(1, len(csv_data)):
-        for i in range(i, interpolation_limit + 1):
-            if not math.isnan(csv_data[row_index][col_index]) and
-            not math.isnan(csv_data[row_index + i][col_index])
-                should_interpolate[row_index] = True
