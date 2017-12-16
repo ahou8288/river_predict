86e8b5bdbd9f6d3ffe98869fdac407de8a2f6741
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-12-16 17:16:38 -0300

    attempting to find valid items for window

--- load_window_data.py
+++ load_window_data.py
@@ -3,7 +3,6 @@ import csv
 import math
 
 # Load all rivers
-# Do an interpolation pass
 # Choose a window
 # Find as much data that fits the window as possible
 # Save in .mat format
@@ -20,4 +19,30 @@ finally:
 for index, entry in enumerate(csv_data):
     csv_data[index][1] = float(entry[1])
     csv_data[index][2] = float(entry[2])
-# print(csv_data)
+for i in csv_data:
+    print(i)
+
+num_levels = 3
+num_rainfall = 5
+
+output = []
+
+for index in range(max(num_rainfall,num_levels),len(csv_data)-1):
+    # Is there a current level
+    if math.isnan(csv_data[index+1][2]):
+        is_valid=True
+        # Is there past rainfall data
+        for i in range(num_rainfall):
+            if math.isnan(csv_data[index-i][1]):
+                is_valid=False
+                break
+        # Is there past level data
+        for i in range(num_levels):
+            if math.isnan(csv_data[index-i][2]):
+                is_valid=False
+                break
+        if is_valid:
+            print(str(index)+' is valid')
+        else:
+            print(str(index)+' is invalid')
+    # break
