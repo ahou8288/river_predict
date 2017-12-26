de7f90694c7d278fec7438d5650694ecdcc9e138
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-12-26 13:24:33 +1100

    Loaded data with dates parsed

--- New Dataset Acquisition/join_csvs.py
+++ New Dataset Acquisition/join_csvs.py
@@ -23,45 +23,23 @@ def process_csv(filename):
     # Try to load in csv file using pandas
     try:
         df = pd.read_csv(csv_folder + filename, skiprows=skip, usecols=[
-            'Date', 'Level (Metres)', 'Discharge (ML/d)'])  # , parse_dates=['Date'])
+            'Date', 'Level (Metres)', 'Discharge (ML/d)'], parse_dates=['Date'])
     except ValueError:  # Data does not have discharge volume
         df = pd.read_csv(csv_folder + filename, skiprows=skip, usecols=[
-            'Date', 'Level (Metres)'])  # , parse_dates=['Date'])
+            'Date', 'Level (Metres)'], parse_dates=['Date'])
     return fix_names(df)
 
 # Read all the csvs
 print('Loading all dataframes.')
 dataframes = Pool(4).map(process_csv, sorted(csv_list))
 
-# for i in dataframes:
-#     print(i.describe())
-
 print('Joining all loaded dataframes.')
 df = pd.concat(dataframes)
 
-counter = 0
-for i in range(0,len(df)):
-    entry = df.iloc[i]
-    if entry.Level and entry.Discharge:
-        print(entry.Level)
-        print(entry.Discharge)
-        counter+=1
-    print('{} Valid {}'.format(entry.Date,not math.isnan(entry.Level) and not math.isnan(entry.Discharge)))
-    break
-print('counter {}'.format(counter))
+print('Sorting based on date.')
+df.sort('Date', axis=1)
 
-# df2 = df.drop('Date', axis=1)
-# print(df2.first_valid_index())
-# print(df2.last_valid_index())
-# print(len(df2))
-
-# print(df2[1000000:1000500])
-
-# print(df)
-# print(df.describe())
-
-# print('Writing to file')
-# pickle_path = str(Path(__file__).parent.parent) + \
-#     '/New Dataset/Pickles/combined_csvs.pickle'
-# df.to_pickle(pickle_path, compression='bz2')
-print('All done.')
+print('Writing to file')
+pickle_path = str(Path(__file__).parent.parent) + \
+    '/New Dataset/Pickles/combined_csvs.pickle'
+df.to_pickle(pickle_path, compression='bz2')
