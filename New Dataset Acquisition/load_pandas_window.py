ac9cc739b3669871a55b78264a956da6f50ecc70
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2017-12-26 21:12:19 +1100

    Created a list of previous levels in a new column

--- New Dataset Acquisition/load_pandas_window.py
+++ New Dataset Acquisition/load_pandas_window.py
@@ -6,14 +6,21 @@ pickle_path = str(Path(__file__).parent.parent) + \
 df = pandas.read_pickle(pickle_path, compression='bz2')
 print('Finished loading')
 
-def newcol(inp,arr):
-    return inp+arr[0]
 print('Creating new column.')
-df = df.assign(Diff=lambda df:
-    newcol(
-        df.Discharge,
-        [df.Discharge.shift(1), df.Discharge.shift(2)]
-        )
-    )
+# Column will have a list with the previous river levels
+previous_levels = 3
+col_names = []
+# Create columns 1 by one
+for i in range(1, previous_levels + 1):
+    col_name = 'previous' + str(i)
+    col_names.append(col_name) # Store column names to use then remove them later.
+    df = df.assign(**{col_name: df.Discharge.shift(i)})
+# Aggregate columns into a list
+df['past_levels'] = df[col_names].values.tolist()
+# Drop temporary columns
+df.drop(col_names, inplace=True, axis=1)
 
+print('Columns created.')
 print(df.iloc[10:20])
+
+df.set_index('Date', inplace=True)
