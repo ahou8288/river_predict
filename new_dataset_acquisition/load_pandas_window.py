c03c1dbc84bd167cdbe89c421096f2dd73ba7839
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-01 15:01:30 +1100

    Put column aggregation in it's own function.

--- new_dataset_acquisition/load_pandas_window.py
+++ new_dataset_acquisition/load_pandas_window.py
@@ -1,9 +1,31 @@
 import pandas
 pandas.set_option('display.width', 200)
-# pandas.set_option('display.max_rows', 500)
+pandas.set_option('display.max_rows', 500)
 from pathlib import Path
 steps_der_day = 96
 
+
+def create_history_column(col_name, num_timesteps, input_df, verbose=False):
+    if verbose:
+        print('Creating column history for {} with {} time steps.'.format(
+            col_name, num_timesteps))
+    new_col_name = 'Past_' + col_name.lower()
+    temp_names_list = []
+    # Create columns 1 by one
+    for i in range(1, previous_levels + 1):
+        if verbose and i % 20 == 0:
+            print('Created {} columns.'.format(i))
+        temp_col_name = 'prev' + str(i)
+        # Store column names to use then remove them later.
+        temp_names_list.append(temp_col_name)
+        input_df = input_df.assign(
+            **{temp_col_name: input_df[col_name].shift(i)})
+        input_df[temp_col_name] = input_df[temp_col_name].round(3)
+    # Aggregate columns into a list
+    input_df[new_col_name] = input_df[temp_names_list].values.tolist()
+    # Drop temporary columns
+    input_df.drop(temp_names_list, inplace=True, axis=1)
+    return input_df
 
 pickle_path = str(Path(__file__).parent.parent) + \
     '/new_dataset/Pickles/combined_csvs.pickle'
@@ -31,22 +53,13 @@ full_df['Rainfall'].fillna(
 full_df['Rainfall'] /= steps_der_day
 
 
-print('Creating new column.')
+print('Creating new columns with history.')
 # Column will have a list with the previous river levels
-previous_levels = 3
-col_names = []
-# Create columns 1 by one
-for i in range(1, previous_levels + 1):
-    col_name = 'previous' + str(i)
-    # Store column names to use then remove them later.
-    col_names.append(col_name)
-    full_df = full_df.assign(**{col_name: full_df.Discharge.shift(i)})
-# Aggregate columns into a list
-full_df['past_levels'] = full_df[col_names].values.tolist()
-# Drop temporary columns
-full_df.drop(col_names, inplace=True, axis=1)
+previous_levels = 500
+previous_rainfalls = 500
+full_df = create_history_column('Rainfall', previous_rainfalls, full_df, True)
+full_df = create_history_column('Discharge', previous_levels, full_df, True)
 print('Columns created.')
-
 
 print('Printing dataframe.')
 print(full_df.iloc[1000:1500])
