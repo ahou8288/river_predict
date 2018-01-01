fc4a9f0443019a506294e99d961dceb1016824af
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-01 11:01:46 +1100

    Changed csv date to D/M/Y instead of comma seperated numbers for each part of the date

--- sql_snippets/nymboida_rainfall.py
+++ sql_snippets/nymboida_rainfall.py
@@ -5,11 +5,20 @@ import csv
 conn = sqlite3.connect('../old_dataset/mydb.db')
 c = conn.cursor()
 
+print('Executing SQL.')
 c.execute('SELECT year, month, day, rainfall FROM rainfall WHERE station_id = '+str(58045))
 
 data = c.fetchall()
-import csv
 
+for i in range(len(data)):
+    data[i] = [
+        '{}/{}/{}'.format(data[i][2],data[i][1],data[i][0]),
+        data[i][3]
+    ]
+
+print('Writing to file.')
 with open('../old_dataset/nymboida_rain.csv', 'w') as myfile:
-    wr = csv.writer(myfile, quoting=csv.QUOTE_NONE)
+    wr = csv.writer(myfile)
     wr.writerows(data)
+
+print('Finished.')
