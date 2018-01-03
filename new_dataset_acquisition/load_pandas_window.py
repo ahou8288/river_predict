ebe909fa7a99861fb05bc96d4f88e072709c30fa
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-03 14:33:54 +1100

    Moved a few tasks out of loops, included header row.

--- new_dataset_acquisition/load_pandas_window.py
+++ new_dataset_acquisition/load_pandas_window.py
@@ -6,21 +6,6 @@ from tqdm import tqdm
 steps_der_day = 96
 
 
-def create_history_column(col_name, num_timesteps, input_df):
-    # print('Creating column history for {} with {} time steps.'.format(
-    #     col_name, num_timesteps))
-    input_df = input_df.assign(**{col_name.lower() + '_' + str(i): input_df[
-                               col_name].shift(i) for i in range(1, previous_levels + 1)})
-    return input_df
-
-
-def write_chunk_to_csv(small_df):
-    small_df = create_history_column(
-        'Rainfall', previous_rainfalls, small_df)
-    small_df = create_history_column(
-        'Level', previous_levels, small_df)
-    small_df.to_csv(csv_filename, header=False, mode='a', index=False)
-
 pickle_path = '../new_dataset/Pickles/combined_csvs.pickle'
 df = pandas.read_pickle(pickle_path, compression='bz2')
 print('Finished loading')
@@ -51,9 +36,10 @@ print('Creating new columns with history.')
 previous_levels = 700
 previous_rainfalls = previous_levels
 
-num_chunks = 40
+num_chunks = 100
 
-csv_filename = '../new_dataset/nymboida_gaussian_week.csv'
+csv_filename = '../new_dataset/nymboida_gaussian_{}steps.csv'.format(
+    previous_levels)
 
 # to make life easier
 try:
@@ -66,7 +52,32 @@ full_df.dropna(inplace=True)
 del full_df['Date']
 del full_df['Discharge']
 
+
+def create_history_column(col_name, num_timesteps, input_df):
+    # print('Creating column history for {} with {} time steps.'.format(
+    #     col_name, num_timesteps))
+    input_df = input_df.assign(**{col_name.lower() + '_' + str(i): input_df[
+                               col_name].shift(i) for i in range(1, previous_levels + 1)})
+    return input_df
+
+
+def write_chunk_to_csv(small_df):
+    small_df = create_history_column(
+        'Rainfall', previous_rainfalls, small_df)
+    small_df = create_history_column(
+        'Level', previous_levels, small_df)
+    small_df.to_csv(csv_filename, header=False, mode='a', index=False)
+
+# write header row manually
+with open(csv_filename, 'w') as f:
+    col_list = ['Rainfall', 'Level', ] + \
+        ['rainfall' + str(i) for i in range(1, previous_rainfalls + 1)] + \
+        ['level' + str(i) for i in range(1, previous_levels + 1)]
+    f.write(','.join(col_list))
+
+# write in data
 chunks = np.array_split(full_df, num_chunks)
 with Pool(4) as p:
     for _ in tqdm(p.imap_unordered(write_chunk_to_csv, chunks), total=num_chunks, unit='chunk'):
         pass
+
