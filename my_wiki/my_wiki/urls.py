99da3dab0f591b1b208e432eadced38a58ad110e
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-05 11:25:35 +1100

    Redirected root url

--- my_wiki/my_wiki/urls.py
+++ my_wiki/my_wiki/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('guide/')),
     url(r'^admin/', admin.site.urls),
     url(r'^wiki/', include('waliki.urls')),
     url(r'^guide/', include('guide.urls')),
