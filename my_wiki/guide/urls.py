c4fbf2334b1646a518eb423a73e45f74024827a1
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-05 15:57:58 +1100

    Set up to have a list of rivers and embedded google maps iframes for each section

--- my_wiki/guide/urls.py
+++ my_wiki/guide/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$levels', views.levels, name='levels'),
+    url(r'levels', views.levels, name='levels'),
+    url(r'map_list', views.map_list, name='map_list'),
+    url(r'map_view/(.*)$', views.map_view, name='map_view'),
 ]
--- my_wiki/guide/views.py
+++ my_wiki/guide/views.py
@@ -4,10 +4,28 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'home.html')
-    
+
+
 def levels(request):
     rivers = [
         {'name': 'Nymboida', 'level': 1.3},
         {'name': 'Barrington', 'level': 2.0},
     ]
-    return render(request, 'levels.html', {'rivers':rivers})
+    return render(request, 'levels.html', {'rivers': rivers})
+
+maps = [{
+    'name': 'Gwydir River',
+    'url': 'gwydir',
+    'embed': 'https://www.google.com/maps/d/u/0/embed?mid=1wZIXpfFOVdZweF1In8VUwOP1LwFyg9Dp',
+}]
+
+
+def map_list(request):
+    return render(request, 'map_list.html', {'maps': maps})
+
+
+def map_view(request, river_name):
+    for i in maps:
+        if river_name == i['url']:
+            return render(request, 'map_view.html', {'url': i['embed'], 'name': river_name})
+    print('Not found')
