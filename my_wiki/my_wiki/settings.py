ec28b7172a2f32605481025402cc00f50f760865
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-10 09:49:02 +1100

    Models added

--- my_wiki/my_wiki/settings.py
+++ my_wiki/my_wiki/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rivers',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'my_wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['rivers/templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- my_wiki/rivers/models.py
+++ my_wiki/rivers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class River(models.Model):
+    name = models.CharField(max_length=100)
+    level_url = models.URLField()
+    river_id = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    minimum = models.FloatField()
+    def __str__(self):
+        return self.name
