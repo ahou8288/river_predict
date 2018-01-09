a6f0d6ee2056eb4a2b017573d75aa9ed77fa150b
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-09 21:50:15 +1100

    Did some daily data for johannes

--- new_dataset_acquisition/load_daily_data.py
+++ new_dataset_acquisition/load_daily_data.py
@@ -22,29 +22,29 @@ full_df = full_df.sort_values('Date')
 full_df = full_df.round(4)
 del full_df['Discharge']
 
-full_df = full_df.assign(
-    **{'diff': full_df['Date'] - full_df['Date'].shift(1)})
+# full_df = full_df.assign(
+#     **{'diff': full_df['Date'] - full_df['Date'].shift(1)})
 
-longest_section = 0
-longest_section_end = 0
-current_section = 0
+# longest_section = 0
+# longest_section_end = 0
+# current_section = 0
 
-for ind, entry in full_df.iterrows():
-    if entry['diff'] == pandas.Timedelta('1 days'):
-        current_section += 1
-    else:
-        if current_section > longest_section:
-            longest_section = current_section
-            longest_section_end = ind
-        current_section = 0
+# for ind, entry in full_df.iterrows():
+#     if entry['diff'] == pandas.Timedelta('1 days'):
+#         current_section += 1
+#     else:
+#         if current_section > longest_section:
+#             longest_section = current_section
+#             longest_section_end = ind
+#         current_section = 0
 
-print('Longest section is {} long. Finishes at {}'.format(
-    longest_section, longest_section_end))
+# print('Longest section is {} long. Finishes at {}'.format(
+#     longest_section, longest_section_end))
 
-cont = full_df.iloc[longest_section_end-longest_section:longest_section_end]
+# cont = full_df.iloc[longest_section_end-longest_section:longest_section_end]
 
-print(cont[cont['diff'] != pandas.Timedelta('1 days')])
+# print(cont[cont['diff'] != pandas.Timedelta('1 days')])
 
-# print(full_df)
+# # print(full_df)
 
-# full_df.to_csv('../new_dataset/daily_nymbo.csv',header=False,index=False)
+full_df.to_csv('../new_dataset/daily_nymbo.csv',header=False,index=False)
