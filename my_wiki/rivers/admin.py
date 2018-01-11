54efb37e2fa1c806c7337c6da99f9a18dd9f989b
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-11 12:42:52 +1100

    Added some NSW rivers

--- my_wiki/rivers/admin.py
+++ my_wiki/rivers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import River
+from .models import River, Section
 
 admin.site.register(River)
+admin.site.register(Section)
--- my_wiki/rivers/views.py
+++ my_wiki/rivers/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .models import River, Section
 
 # Create your views here.
+
 
 def index(request):
     return render(request, 'home.html')
 
+
 def rivers(request):
-    return render(request, 'rivers.html')
+    output = []
+    rivers = River.objects.all()
+    for river in rivers:
+        sections = Section.objects.filter(river=river)
+        output.append({
+            'name':river.name,
+            'sections':sections
+            })
+    return render(request, 'rivers.html', {'rivers': output})
+
 
 def levels(request):
     return render(request, 'levels.html')
