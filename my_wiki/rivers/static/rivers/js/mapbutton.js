ab6f281835e55a4597e5608ab0a0d8ed7d00df84
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-24 09:42:23 +1100

    Moved css styling out of javascript

--- my_wiki/rivers/static/rivers/js/mapbutton.js
+++ my_wiki/rivers/static/rivers/js/mapbutton.js
@@ -1,38 +1,24 @@
 function CenterControl(controlDiv, functionList) {
-  // Set CSS for the control border.
-  var controlUI = document.createElement('div');
-  controlUI.style.backgroundColor = '#fff';
-  controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
-  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-  controlUI.style.cursor = 'pointer';
-  controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to add a marker to the map';
-  controlDiv.appendChild(controlUI);
+    var controlUI = document.createElement('div');
+    controlUI.classList.add('controlUI');
+    controlUI.title = 'Click to add a marker to the map';
+    controlDiv.appendChild(controlUI);
 
-  var headerText = document.createElement('div');
-  headerText.innerHTML = 'Add locations to map';
-  controlUI.appendChild(headerText);
+    var headerText = document.createElement('div');
+    headerText.innerHTML = 'Add locations to map';
+    controlUI.appendChild(headerText);
 
+    var functionRef;
 
-  var functionRef;
-  var index = 0;
-  for (var text in functionList) {
-    functionRef = functionList[text];
-  // Set CSS for the control interior.
-    controlText = document.createElement('span');
-    controlText.style.color = 'rgb(25,25,25)';
-    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-    controlText.style.fontSize = '16px';
-    controlText.style.lineHeight = '38px';
-    controlText.style.paddingLeft = '5px';
-    controlText.style.paddingRight = '5px';
-    controlText.style.paddingBottom = '10px';
+    for (var text in functionList) {
+        functionRef = functionList[text];
+        controlText = document.createElement('span');
+        controlText.classList.add('controlText');
 
-    controlText.innerHTML = text;
-    controlText.setAttribute('onclick', functionRef+'();');
+        controlText.innerHTML = text;
+        controlText.setAttribute('onclick', functionRef+'();');
 
-    controlUI.appendChild(controlText);
-    index += 1;
-  }
+        controlUI.appendChild(controlText);
+
+    }
 }
