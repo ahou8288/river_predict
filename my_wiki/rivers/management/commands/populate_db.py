f66b1861e2f6db77297cd1563f57b21192865658
Andrew Houghton <ahou8288@uni.sydney.edu.au>
2018-01-30 15:38:20 +1100

    Wrote another web scraper for dealing with points

--- my_wiki/rivers/management/commands/populate_db.py
+++ my_wiki/rivers/management/commands/populate_db.py
@@ -1,23 +1,30 @@
 from django.core.management.base import BaseCommand
 from rivers.models import River
 import json
-
+import os
+from pprint import pprint
 
 class Command(BaseCommand):
     args = ''
     help = 'Loads data'
 
     def _load_rivers(self):
-        with open('', 'r') as handle:
-            data = json.load(handle)
+        filepath = 'rivers/management/commands/json'
+        filelist = os.listdir(filepath)
 
-        for river in data:
+        for filename in filelist:
+            with open(filepath + '/' + filename, 'r') as handle:
+                river = json.load(handle)
+            pprint(river)
             river_name = river['WATERWAY']
+
+            #Create river if required
             if River.objects.filter(name=river_name).count() == 0:
                 new_river = River(name = river_name, description = '' )
-            print('{} has been added.'.format(river_name))
-            break
-        print('loading rivers')
+                new_river.save()
+                print('New river created')
+            else:
+                print('River already exists')
 
     def handle(self, *args, **options):
         self._load_rivers()
