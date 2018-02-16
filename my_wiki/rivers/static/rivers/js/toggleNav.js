c46f47844e7e2cc1149e65d18134369ecd7cedbd
Andrew Houghton <houghtonandrew0@gmail.com>
2018-02-17 09:59:52 +1100

    Menu looks alright. Now to work on pages for mobile and adding the logo back in

--- my_wiki/rivers/static/rivers/js/toggleNav.js
+++ my_wiki/rivers/static/rivers/js/toggleNav.js
@@ -1,14 +1,30 @@
 var navOpen=false;
 function toggleNav() {
     if (navOpen){
-        document.getElementById("myNav").style.minWidth = "0%";
-        document.getElementById("navbutton").classList.remove('active')
-        document.getElementById("pagebutton").classList.remove('active')
+        document.getElementById("myNav").classList.remove('active');
+        buttons = document.getElementsByClassName("closebtn")
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.remove('active');
+        }
         navOpen=false
     } else {
-        document.getElementById("myNav").style.minWidth = "30em";
-        document.getElementById("navbutton").classList.add('active')
-        document.getElementById("pagebutton").classList.add('active')
+        document.getElementById("myNav").classList.add('active');
+        buttons = document.getElementsByClassName("closebtn")
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].classList.add('active');
+        }
         navOpen=true;
     }
 }
+
+function clickHandler(e){
+    classes = e.target.classList;
+    if (!classes.contains("closebtn")){
+           if (navOpen){
+            toggleNav();
+        }
+    }
+}
+
+var element = document.getElementById('wholepage');
+element.addEventListener('click', clickHandler);
